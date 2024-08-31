80a399ee4c58d1477138021512b4d7bb4d501677
ankhbayar sumiyabayar <75901266+angaa123@users.noreply.github.com>
2024-09-01 06:31:50 +0800

    header setup

--- front_end/src/app/page.jsx
+++ front_end/src/app/page.jsx
@@ -1,10 +1,11 @@
 // import LogInPage from "@/app/logInPage"
 // import SignUpPage from "@/app/signUpPage";
+import Index from "@/components/index";
 
 export default function Home() {
   return (
     <>
-
+    <Index/>
     </>
   );
 }
---
+++ front_end/src/components/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './header'
+function Index() {
+    return (
+        <>
+        <Header/>
+        </>
+    )
+}
+
+export default Index
